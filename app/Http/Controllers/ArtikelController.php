de79ec27f423099b144bd821c1274c6b71003990
NasywanHanif <nasywanh10@gmail.com>
2023-04-19 00:24:16 +0700

    update artikell controller (for edit and show) #6 #8

--- app/Http/Controllers/ArtikelController.php
+++ app/Http/Controllers/ArtikelController.php
@@ -25,5 +25,27 @@ class ArtikelController extends Controller
         ]);
         return redirect('/artikel');
     }
-    
+    public function show($artikel_id)
+    {
+        $artikel = Artikel::find($artikel_id);
+        return view('informasi_stunting.informasi_stunting_show', compact('artikel'));
+    }
+    public function edit($artikel_id)
+    {
+        $artikel = Artikel::find($artikel_id);
+        return view('informasi_stunting.informasi_stunting_edit', compact('artikel'));
+    }
+    public function update(Request $request, $artikel_id)
+    {
+        Artikel::find($artikel_id)->update([
+            'judul' => $request -> judul,
+            'isi' => $request -> isi,
+        ]);
+        return redirect('/artikel');
+    }
+    public function destroy($artikel_id)
+    {
+        Artikel::find($artikel_id)->delete();
+        return redirect('/artikel');
+    }
 }
