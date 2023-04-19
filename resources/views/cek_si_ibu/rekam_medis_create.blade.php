cdeef0d6c8b67cbedaee26ac34fc0776a637afdd
najlalya <najlalya@student.telkomuniversity.ac.id>
2023-04-20 04:05:10 +0700

    resolve rekam medis #15

--- resources/views/cek_si_ibu/rekam_medis_create.blade.php
+++ resources/views/cek_si_ibu/rekam_medis_create.blade.php
@@ -30,6 +30,74 @@
                             </div>
                         </div>
                     </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Tanggal Rekam Medis</h5>
+                                <h1><input type="date" class="form-control-plaintext" value="" name="tanggal_rekam_medis"></h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Berat Janin (g)</h5>
+                                <h1><input type="int" class="form-control-plaintext" value="" name="berat_janin" placeholder="..."></h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Panjang Janin (cm)</h5>
+                                <h1><input type="int" class="form-control-plaintext" value="" name="panjang_janin" placeholder="..."></h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Detak Jantung Janin (bpm)</h5>
+                                <h1><input type="int" class="form-control-plaintext" value="" name="detak_janin" placeholder="..."></h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                    <h2><b>Ibu</b></h2></a>
+                </div>
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Berat Ibu (kg)</h5>
+                                <h1><input type="int" class="form-control-plaintext" value="" name="berat_ibu" placeholder="..."></h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Detak Jantung Ibu (bpm)</h5>
+                                <h1><input type="int" class="form-control-plaintext" value="" name="detak_ibu" placeholder="..."></h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-md-12">
+                        <button type="submit" class="btn btn-primary">
+                            Tambah
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 @endsection
