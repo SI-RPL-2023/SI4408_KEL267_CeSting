de68666f33371d388396301c90271a5d3123b8d8
najlalya <najlalya@student.telkomuniversity.ac.id>
2023-04-19 15:22:32 +0700

    Upload rekam_medis_create #15

---
+++ resources/views/cek_si_ibu/rekam_medis_create.blade.php
@@ -0,0 +1,15 @@
+@extends('navbar')
+@section('isihalaman')
+<div class="container mt-5 mb-5">
+    <div class="card row mb-3">
+        <div class="col">
+            <div class="d-flex flex-column align-items-center text-center p-3 py-5">
+                <img class="img-fluid" width="150px" src="{{asset('assets/images/Icon-Si_Ibu.png')}}">
+                <h1><span class="font-weight-bold"><br>Rekam Medis</span></h1>
+            </div>
+        </div>
+    </div>
+  
+    
+</div>
+@endsection
