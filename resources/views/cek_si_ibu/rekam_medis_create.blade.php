c493cc01849493ed9500a9b2a82b7d11787f49c0
najlalya <najlalya@student.telkomuniversity.ac.id>
2023-04-19 16:45:59 +0700

    update rekam medis #15

--- resources/views/cek_si_ibu/rekam_medis_create.blade.php
+++ resources/views/cek_si_ibu/rekam_medis_create.blade.php
@@ -9,7 +9,27 @@
             </div>
         </div>
     </div>
-  
-    
+    @if($errors->any())
+        @foreach($errors->all() as $err)
+            <div class="alert alert-danger my-5" role="alert">{{ $err }}</div>
+        @endforeach
+    @endif
+    <div class="card row mb-3">
+        <div class="p-3 py-5">
+            <form action="/rekam_medis" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                    <h2><b>Janin</b></h2>
+                </div>
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Username Ibu</h5>
+                                <h1><input type="text" class="form-control-plaintext" value="" name="username" placeholder="..."></h1>
+                            </div>
+                        </div>
+                    </div>
+    </div>
 </div>
 @endsection
