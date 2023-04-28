ed37578b094da4d3400859f8e9ea5ae31cdd7d80
NasywanHanif <nasywanh10@gmail.com>
2023-04-29 06:37:08 +0700

    update tampilan home (home blade)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,7 @@
                                     <div class="card-body">
                                         <h2 class="card-title">{{$artikel -> judul}}</h2>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-                                            <a href="/artikel/{{$artikel -> id}}/edit"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></a>
+                                            <a href="/artikel/{{$artikel -> id}}"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></a>
                                         </svg>
                                     </div>
                                 </div>
@@ -41,7 +41,7 @@
                                     <div class="card-body">
                                         <h2 class="card-title">{{$event -> judul}}</h2>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-                                            <a href="/event/{{$event -> id}}/edit"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></a>
+                                            <a href="/event/{{$event -> id}}"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></a>
                                         </svg>
                                     </div>
                                 </div>
